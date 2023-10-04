4e50973e81f6713b33a5717e81d59e5ae6a46834
Bernhard Wallisch <walliscb@technikum-wien.at>
2023-10-04 19:08:50 +0200

    created 2 users

--- FirstConsoleApp/src/main/java/at/fhtw/slm/consoleapp/MyConsoleApp.java
+++ FirstConsoleApp/src/main/java/at/fhtw/slm/consoleapp/MyConsoleApp.java
@@ -3,5 +3,8 @@ package at.fhtw.slm.consoleapp;
 public class MyConsoleApp {
     public static void main(String[] args) {
         System.out.println("A big hello to ....");
+
+        User user = new User(1, "Rudi", "Ratlos");
+        var user2 = new User(2, "Susi", "Sorglos");
     }
 }
