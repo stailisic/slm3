c4621b41f2a1945ae077d1954d87581469599f9c
Bernhard Wallisch <walliscb@technikum-wien.at>
2023-10-04 18:51:36 +0200

    a better hello message

--- FirstConsoleApp/src/main/java/at/fhtw/slm/consoleapp/MyConsoleApp.java
+++ FirstConsoleApp/src/main/java/at/fhtw/slm/consoleapp/MyConsoleApp.java
@@ -2,6 +2,6 @@ package at.fhtw.slm.consoleapp;
 
 public class MyConsoleApp {
     public static void main(String[] args) {
-        System.out.println("Hello Git!");
+        System.out.println("A big hello to ....");
     }
 }
