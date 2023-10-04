a5c9406df790a4ff6b1858da2cb919466bbe4227
Bernhard Wallisch <walliscb@technikum-wien.at>
2023-10-04 20:08:11 +0200

    Update MyConsoleApp.java
    conflicting line (A)

--- FirstConsoleApp/src/main/java/at/fhtw/slm/consoleapp/MyConsoleApp.java
+++ FirstConsoleApp/src/main/java/at/fhtw/slm/consoleapp/MyConsoleApp.java
@@ -2,7 +2,7 @@ package at.fhtw.slm.consoleapp;
 
 public class MyConsoleApp {
     public static void main(String[] args) {
-        System.out.println("A big hello to ....");
+        System.out.println("A big hello to BIC3-SLM");
 
         User user = new User(1, "Rudi", "Ratlos");
         var user2 = new User(2, "Susi", "Sorglos");
